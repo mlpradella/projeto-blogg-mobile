39a240ba88b31dbf8b0c253eac8b937dffa1bb98
Bianca-a-a <bianca.gastaldim@yahoo.com>
2025-10-16 11:53:45 -0300

    coloquei a opniao do lado da img

--- mobile/app/(tabs)/acao.tsx
+++ mobile/app/(tabs)/acao.tsx
@@ -15,7 +15,7 @@ export default function HomeScreen() {
           </ThemedView>
     
           <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">Tropa de elite</ThemedText>
+            <ThemedText type="subtitle" style={styles.titulo1}>Tropa de elite</ThemedText>
           </ThemedView>
 
           <View style={styles.imageRow}>
@@ -29,14 +29,29 @@ export default function HomeScreen() {
   />
 </View>
 
+      <View style={styles.sinopse1coluna}>
+         <ThemedText type='default' style={styles.sinopse1}>
+         Em "Tropa de Elite", o Capitão Nascimento, do Batalhão de Operações Especiais (BOPE),
+         precisa encontrar um substituto enquanto comanda a missão de pacificar uma favela. Durante a operação, 
+         ele resgata dois aspirantes a oficiais, Neto e Matias, que, inspirados, buscam entrar para a sua equipe.
+        </ThemedText>
+        </View>
+
           <ThemedView style={styles.texto}>
             <ThemedText style={styles.opiniao1}
-            type="default">O filme vai muito além de ser apenas um filme de ação ele é uma análise profunda sobre corrupção, violência e os dilemas morais enfrentados pelos policiais.
-            O que realmente marca o filme é a narração de Wagner Moura como o Capitão Nascimento. Sua voz, direta e incisiva, nos guia por um universo de decisões extremas, onde não há espaço para fraquezas. As cenas de treinamento do BOPE são intensas e brutalmente realistas, mostrando o nível de pressão psicológica e física que os aspirantes a oficiais precisam suportar.</ThemedText>
+            type="default">O filme vai muito além de ser apenas um filme de ação ele é uma análise profunda sobre corrupção,
+             violência e os dilemas morais enfrentados pelos policiais.
+            O que realmente marca o filme é a narração de Wagner Moura como o Capitão Nascimento. 
+            Sua voz, direta e incisiva, nos guia por um universo de decisões extremas, onde não há espaço para fraquezas.
+             As cenas de treinamento do BOPE são intensas e brutalmente realistas, mostrando o nível de pressão psicológica 
+             e física que os aspirantes a oficiais precisam suportar.
+             </ThemedText>
+
+             <ThemedText  style={styles.data1} type='default'> 04/06/2025</ThemedText>
           
           </ThemedView>
           <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">Busca Implacável</ThemedText>
+            <ThemedText type="subtitle" style={styles.titulo2}>Busca Implacável</ThemedText>
           </ThemedView>
 
           <View style={styles.imageRow}>
@@ -50,6 +65,13 @@ export default function HomeScreen() {
   />
 </View>
 
+    <View style={styles.sinopse2coluna}>
+         <ThemedText type='default' style={styles.sinopse2}>
+         Bryan Mills, um ex-agente do governo com habilidades especiais, precisa usar seu passado para resgatar sua filha, Kim,
+        que é sequestrada por traficantes em Paris. Ele tem pouco tempo para encontrá-la antes que ela seja vendida.
+        </ThemedText>
+        </View>
+
         </View>
         </ScrollView>
         </SafeAreaView>
@@ -61,6 +83,7 @@ export default function HomeScreen() {
         flex: 1,
         backgroundColor: '#00002B', 
       },
+
 
       titulo: {
         alignItems: 'center',
@@ -77,6 +100,14 @@ export default function HomeScreen() {
         marginBottom: -15,
       },
 
+      titulo1: {
+        color: '#3d723f',
+      },
+      
+      titulo2: {
+        color: '#3d723f',
+      },
+
       stepContainer: {
         gap: 8,
         marginBottom: 8,
@@ -85,10 +116,36 @@ export default function HomeScreen() {
         marginTop: 15,
       },
 
+      sinopse1coluna:{
+       flex: 1,
+       justifyContent: 'flex-start',
+       marginTop: -181,
+      },
+
+      sinopse1:{
+        fontSize: 13,
+        maxWidth: 190,
+        lineHeight: 15,
+        marginLeft: 175,
+      },
+
+      sinopse2:{
+        fontSize: 13,
+        maxWidth: 190,
+        lineHeight: 14,
+        marginLeft: 175,
+      },
+
+      sinopse2coluna:{
+        flex: 1,
+        justifyContent: 'flex-start',
+        marginTop: -159,
+      },
+
       opiniao1: {
         fontSize:14,
         marginLeft: 10, 
-        marginTop: -35,
+        marginTop: 1,
         lineHeight: 21,
         maxWidth: 345,
       },
@@ -123,9 +180,16 @@ export default function HomeScreen() {
         width: 120,
         resizeMode: 'contain',
         marginLeft: -190,
-        marginTop: -90,
+        marginTop: -97,
         alignItems: 'flex-start',
       },
+
+      data1:{
+        color: '#3d723f',
+        marginTop: 20,
+        marginLeft: 252,
+      },
+
       capa2busca:{
         height: 250,
         width: 150,
@@ -135,12 +199,13 @@ export default function HomeScreen() {
         marginLeft: 10,
         alignItems: 'flex-start',
       },
+
       avaliacao2busca:{
         height: 250,
         width: 100,
         resizeMode: 'contain',
         marginLeft: -190,
-        marginTop: -68,
+        marginTop: -80,
         alignItems: 'flex-start',
       },
     }
