4f920120cf4fb530966f965f21ae727f4e7209d2
Alanzouka <semata654321@gmail.com>
2025-10-24 11:08:29 -0300

    Terminei tudo

--- mobile/app/(tabs)/drama.tsx
+++ mobile/app/(tabs)/drama.tsx
@@ -5,276 +5,359 @@ import { ThemedText } from '@/components/themed-text';
 
 export default function HomeScreen() {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <ThemedText type="title" style={styles.titleText}>DRAMA</ThemedText>
+    <ScrollView style={estilos.container} contentContainerStyle={estilos.containerConteudo}>
+      <ThemedText type="title" style={estilos.textoTituloPrincipal}>DRAMA</ThemedText>
 
-      <ThemedText type="subtitle" style={styles.movieTitle}>As vantagens de ser invisível</ThemedText>
+      {/* ======================================================= */}
+      {/* FILME 1: As Vantagens de ser Invisível                  */}
+      {/* ======================================================= */}
+      <ThemedText type="subtitle" style={estilos.tituloFilme}>As vantagens de ser invisível</ThemedText>
 
-     
-      <View style={styles.posterAndDetailsRow}>
+      <View style={estilos.linhaPosterDetalhes}>
         
-        <View style={styles.posterColumn}>
+        <View style={estilos.colunaPoster}>
           <Image
             source={require('@/assets/images/vant.png')}
-            style={styles.posterImage}
+            style={estilos.imagemPoster}
+            contentFit="contain"
           />
         </View>
 
-      
-        <View style={styles.detailsColumn}>
+        <View style={estilos.colunaDetalhes}>
           
-          <View style={styles.ratingRow}>
-            <Image source={require('@/assets/images/12.png')} style={styles.ageRatingImage} />
-            <ThemedText type="defaultSemiBold" style={styles.yearText}>2012</ThemedText>
+          <View style={estilos.linhaClassificacaoAno}>
+            <Image source={require('@/assets/images/12.png')} style={estilos.imagemClassificacaoIdade} />
+            <ThemedText type="defaultSemiBold" style={estilos.textoAno}>2012</ThemedText>
           </View>
 
-          <View style={styles.starsAndScoreRow}>
-            <View style={styles.starsContainer}>
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/group.png')} style={styles.starImage} /> 
+          <View style={estilos.linhaEstrelasPontuacao}>
+            <View style={estilos.containerEstrelas}>
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/group.png')} style={estilos.imagemEstrela} /> 
             </View>
-            <ThemedText type="subtitle" style={styles.scoreText}>4.5</ThemedText>
+            <ThemedText type="subtitle" style={estilos.textoPontuacao}>4.5</ThemedText>
           </View>
         
-          <View style={styles.durationRow}>
-            <Image source={require('@/assets/images/relogio.png')} style={styles.durationIcon} />
-            <ThemedText type="defaultSemiBold" style={styles.durationText}>1h 43min</ThemedText>
+          <View style={estilos.linhaDuracao}>
+            <Image source={require('@/assets/images/relogio.png')} style={estilos.iconeDuracao} />
+            <ThemedText type="defaultSemiBold" style={estilos.textoDuracao}>1h 43min</ThemedText>
           </View>
           
-          <ThemedText type="subtitle" style={styles.sinopseTitle}>Sinopse:</ThemedText>
-          <ThemedText type="defaultSemiBold" style={styles.sinopseBody}>
+          <ThemedText type="subtitle" style={estilos.tituloSinopse}>Sinopse:</ThemedText>
+          <ThemedText type="defaultSemiBold" style={estilos.corpoSinopse}>
             Charlie é um jovem que tem dificuldades para interagir em sua nova escola. Ele se sente deslocado no ambiente. Seu professor de literatura, no entanto, acredita nele e o vê como um gênio.
           </ThemedText>
         </View>
       </View>
       
 
-      <ThemedText type="subtitle" style={styles.reviewTitle}>Resenha:</ThemedText>
-      <ThemedText type="defaultSemiBold" style={styles.reviewBody}>
+      <ThemedText type="subtitle" style={estilos.tituloResenha}>Resenha:</ThemedText>
+      <ThemedText type="defaultSemiBold" style={estilos.corpoResenha}>
         Ver esse filme durante a adolescência é uma experiencia surreal, te fazendo refletir em diversos aspectos, com ótimos atores que me identifiquei, chorando muito com o fim. Simplesmente um dos meus favoritos
       </ThemedText>
-      <ThemedText type="defaultSemiBold" style={styles.reviewDate}>18/01/2009</ThemedText>
+      <ThemedText type="defaultSemiBold" style={estilos.dataResenha}>18/01/2009</ThemedText>
 
-      <ThemedText type="subtitle" style={styles.directorTitle}>Diretor</ThemedText>
-      <View style={styles.directorInfoRow}>
+      <ThemedText type="subtitle" style={estilos.tituloDiretor}>Diretor</ThemedText>
+      <View style={estilos.linhaInfoDiretor}>
         <Image
           source={require('@/assets/images/diretor1.png')}
-          style={styles.directorImage}
+          style={estilos.imagemDiretor}
         />
-        <ThemedText style={styles.directorFilmText}> Stephen Chbosky.</ThemedText>
+        <ThemedText style={estilos.textoNomeDiretor}> Stephen Chbosky.</ThemedText>
       </View>
 
 
-      <ThemedText type="subtitle" style={styles.movieTitle1}>Sociedade dos poetas mortos </ThemedText>
+      {/* ======================================================= */}
+      {/* FILME 2: Sociedade dos poetas mortos                    */}
+      {/* ======================================================= */}
+      <ThemedText type="subtitle" style={estilos.tituloFilme1}>Sociedade dos poetas mortos </ThemedText>
 
-      <View style={styles.posterColumn1}>
+      <View style={estilos.colunaPoster1}>
           <Image
             source={require('@/assets/images/sociedade.png')}
-            style={styles.posterImage1}
+            style={estilos.imagemPoster1}
+            contentFit="contain"
           />
         </View>
 
-        <View style={styles.ratingRow1}>
-            <Image source={require('@/assets/images/12.png')} style={styles.ageRatingImage} />
-            <ThemedText type="defaultSemiBold" style={styles.yearText}>1990</ThemedText>
+        <View style={estilos.linhaClassificacaoAno1}>
+            <Image source={require('@/assets/images/12.png')} style={estilos.imagemClassificacaoIdade} />
+            <ThemedText type="defaultSemiBold" style={estilos.textoAno}>1990</ThemedText>
           </View>
 
-          <View style={styles.starsAndScoreRow1}>
-            <View style={styles.starsContainer}>
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/estrela.png')} style={styles.starImage} />
-                <Image source={require('@/assets/images/group.png')} style={styles.starImage} /> 
+          <View style={estilos.linhaEstrelasPontuacao1}>
+            <View style={estilos.containerEstrelas}>
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/group.png')} style={estilos.imagemEstrela} /> 
             </View>
-            <ThemedText type="subtitle" style={styles.scoreText}>4.7</ThemedText>
+            <ThemedText type="subtitle" style={estilos.textoPontuacao}>4.7</ThemedText>
           </View>
 
-          <View style={styles.durationRow1}>
-            <Image source={require('@/assets/images/relogio.png')} style={styles.durationIcon} />
-            <ThemedText type="defaultSemiBold" style={styles.durationText}>2h 8min</ThemedText>
+          <View style={estilos.linhaDuracao1}>
+            <Image source={require('@/assets/images/relogio.png')} style={estilos.iconeDuracao} />
+            <ThemedText type="defaultSemiBold" style={estilos.textoDuracao}>2h 8min</ThemedText>
 
           </View>
 
-          <View style={styles.test}>
-          <ThemedText type="subtitle" style={styles.sinopseTitle}>Sinopse:</ThemedText>
-          <ThemedText type="defaultSemiBold" style={styles.sinopseBody}>
+          <View style={estilos.containerTeste}>
+          <ThemedText type="subtitle" style={estilos.tituloSinopse}>Sinopse:</ThemedText>
+          <ThemedText type="defaultSemiBold" style={estilos.corpoSinopse}>
             Charlie é um jovem que tem dificuldades para interagir em sua nova escola. Ele se sente deslocado no ambiente. Seu professor de literatura, no entanto, acredita nele e o vê como um gênio.
           </ThemedText>
           </View>
-          <ThemedText type="subtitle" style={styles.reviewTitle}>Resenha:</ThemedText>
-          <ThemedText type="defaultSemiBold" style={styles.reviewBody}>
+          <ThemedText type="subtitle" style={estilos.tituloResenha}>Resenha:</ThemedText>
+          <ThemedText type="defaultSemiBold" style={estilos.corpoResenha}>
           Sendo um artista e tendo assistido esse filme, tocou profundamente não apenas meu coração mas como minha alma. Chorei tanto vendo que me 
           desidratei, 
           não recomendo se você ainda desejar ter uma saúde mental intacta.
-      </ThemedText>
-      <ThemedText type="defaultSemiBold" style={styles.reviewDate}>18/01/2009</ThemedText>
-      <ThemedText type="subtitle" style={styles.directorTitle}>Diretor</ThemedText>
-      <View style={styles.directorInfoRow}>
-        <Image
-          source={require('@/assets/images/9PdTBjn8dJqfn3ygKvsCdl9G06J 2.png')}
-          style={styles.directorImage}
-        />
-        <ThemedText style={styles.directorFilmText}> Peter Weir</ThemedText>
+        </ThemedText>
+        <ThemedText type="defaultSemiBold" style={estilos.dataResenha}>18/01/2009</ThemedText>
+        <ThemedText type="subtitle" style={estilos.tituloDiretor}>Diretor</ThemedText>
+        <View style={estilos.linhaInfoDiretor}>
+          <Image
+            source={require('@/assets/images/9PdTBjn8dJqfn3ygKvsCdl9G06J 2.png')}
+            style={estilos.imagemDiretor}
+          />
+          <ThemedText style={estilos.textoNomeDiretor}> Peter Weir</ThemedText>
+        </View>
+
+      {/* ======================================================= */}
+      {/* FILME 3: Stand by me                                    */}
+      {/* ======================================================= */}
+      <ThemedText type="subtitle" style={estilos.tituloFilme}>Stand by me</ThemedText>
+
+      <View style={estilos.colunaPoster2}>
+          <Image
+            source={require('@/assets/images/20028681 1 (5).png')}
+            style={estilos.imagemPoster2}
+            contentFit="contain"
+          />
+        </View>
+  
+      <View style={estilos.containerTeste2}>
+
+        <View style={estilos.linhaClassificacaoAno2}>
+            <Image source={require('@/assets/images/12.png')} style={estilos.imagemClassificacaoIdade} />
+            <ThemedText type="defaultSemiBold" style={estilos.textoAno}>1990</ThemedText>
+          </View>
+          <View style={estilos.linhaEstrelasPontuacao1}>
+
+            <View style={estilos.containerEstrelas}>
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} />
+                <Image source={require('@/assets/images/estrela.png')} style={estilos.imagemEstrela} /> 
+            </View>
+            <ThemedText type="subtitle" style={estilos.textoPontuacao}>5.0</ThemedText>
+          </View>
+
+          <View style={estilos.linhaDuracao1}>
+            <Image source={require('@/assets/images/relogio.png')} style={estilos.iconeDuracao} />
+            <ThemedText type="defaultSemiBold" style={estilos.textoDuracao}>1h 29min</ThemedText>
+          </View>
+
+          <View style={estilos.containerTeste}>
+          <ThemedText type="subtitle" style={estilos.tituloSinopse}>Sinopse:</ThemedText>
+          <ThemedText type="defaultSemiBold" style={estilos.corpoSinopse}>
+          O filme é uma adaptação de uma novela do Stephen King. É uma história de amadurecimento que acompanha quatro amigos na década de 1950. 
+          Após saberem de um corpo de um adolescente que está desaparecido na floresta, eles embarcam em uma jornada para encontrar o corpo.
+          </ThemedText>
+
+          <ThemedText type="subtitle" style={estilos.tituloResenha1}>Resenha:</ThemedText>
+          </View>
+          <ThemedText type="defaultSemiBold" style={estilos.corpoResenha}>
+          Sempre filmes gays nunca tem final feliz, esse filme é uma faca profundamente enfiada em meu estomago que sempre que me 
+          lembro daquele final me faz pensar seriamente se vale viver. O que amo é que mesmo sendo um filme de 1986 é muito superior a 
+          filmes atuais tanto em desenvolvimento tanto na historia. Tão simples, mas ao mesmo tempo tao impactante.
+        </ThemedText>
+        <ThemedText type="defaultSemiBold" style={estilos.dataResenha}>18/01/2009</ThemedText>
+        <ThemedText type="subtitle" style={estilos.tituloDiretor}>Diretor</ThemedText>
+        <View style={estilos.linhaInfoDiretor}>
+          <Image
+            source={require('@/assets/images/9PdTBjn8dJqfn3ygKvsCdl9G06J 3 (1).png')}
+            style={estilos.imagemDiretor}
+          />
+          <ThemedText style={estilos.textoNomeDiretor}> Rob Reiner</ThemedText>
+        </View>
       </View>
 
-      <ThemedText type="subtitle" style={styles.movieTitle}>Stand by me</ThemedText>
-      
-      
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1, 
     backgroundColor: '#00002E', 
   },
-  contentContainer: {
+  containerConteudo: { 
     paddingBottom: 40, 
   },
  
-  titleText: {
+  textoTituloPrincipal: { 
     fontSize: 40,
     textAlign: 'center', 
     color: '#244A80',
-    marginTop: 30,
+    marginTop: 100,
     fontFamily: 'ui-sans-serif',
   },
-  movieTitle: {
+  tituloFilme: { 
     color: '#377A98',
     marginTop: 20,
     marginLeft: 20,
     fontSize: 22,
   },
-  movieTitle1: {
+  tituloFilme1: { 
     color: '#377A98',
     marginTop: 50,
     marginLeft: 20,
     fontSize: 22,
   },
-  posterAndDetailsRow: {
+  linhaPosterDetalhes: { 
     flexDirection: 'row', 
     marginTop: 10,
     paddingHorizontal: 20, 
   },
   
-  posterColumn: {
+  colunaPoster: { 
     flex: 0.45, 
   },
 
-  posterColumn1: {
+  colunaPoster1: { 
     flex: 0.45, 
     marginTop: 30,
   },
-  posterImage: {
+  colunaPoster2: { 
+    marginTop: 30,
+  },
+  imagemPoster: { 
     width: '100%',
     height: 250,
     resizeMode: 'contain',
   },
 
-  posterImage1: {
+  imagemPoster1: { 
+    width: '50%',
+    height: 230,
+    resizeMode: 'contain',
+  },
+  imagemPoster2: { 
     width: '50%',
     height: 230,
     resizeMode: 'contain',
   },
 
-  detailsColumn: {
+  colunaDetalhes: { 
     flex: 0.55, 
     paddingLeft: 10, 
   },
  
-  ratingRow: {
+  linhaClassificacaoAno: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 5,
   },
-  ratingRow1: {
+  linhaClassificacaoAno1: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 5,
     marginLeft: 185,
     marginTop: -100,
   },
-  ageRatingImage: {
+  linhaClassificacaoAno2: { 
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+    marginLeft: 185,
+  },
+  imagemClassificacaoIdade: { 
     width: 30,
     height: 30,
     marginRight: 5,
   },
-  yearText: {
+  textoAno: { 
     color: '#E0E0E0',
     fontSize: 20,
   },
-  starsAndScoreRow: {
+  linhaEstrelasPontuacao: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 5,
   },
-  starsAndScoreRow1: {
+  linhaEstrelasPontuacao1: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 5,
     marginLeft: 185,
     marginTop: 5,
   },
-  starsContainer: {
+  containerEstrelas: { 
     flexDirection: 'row',
     marginRight: 10,
   },
-  starImage: {
+  imagemEstrela: { 
     width: 20,
     height: 20,
     marginRight: 2, 
   },
-  scoreText: {
+  textoPontuacao: { 
     fontSize: 18,
   },
-  durationRow: {
+  linhaDuracao: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 5,
   },
-  durationRow1: {
+  linhaDuracao1: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 5,
     marginLeft: 185,
   },
-  durationIcon: {
+  iconeDuracao: { 
     width: 20,
     height: 20,
     marginRight: 5,
   },
-  durationText: {
+  textoDuracao: { 
     fontSize: 17,
   },
 
-  sinopseTitle: {
+  tituloSinopse: { 
     color: '#377A98',
     marginTop: 10,
     fontSize: 18,
   },
-  sinopseBody: {
+  corpoSinopse: { 
     color: '#D2FFF4',
     marginTop: 5,
     fontSize: 12,
   },
  
-  reviewTitle: {
+  tituloResenha: { 
     color: '#377A98',
     marginTop: -10,
     marginLeft: 20,
   },
-  reviewBody: {
+  tituloResenha1: { 
+    color: '#377A98',
+    marginTop: -10,
+    marginLeft: -170,
+  },
+  corpoResenha: { 
     color: '#D2FFF4',
     marginHorizontal: 20, 
     marginTop: 10,
     fontSize: 12,
   },
-  reviewDate: {
+  dataResenha: { 
     color: '#69C6AF',
     fontSize: 12,
     marginTop: 5,
@@ -282,29 +365,31 @@ const styles = StyleSheet.create({
     textAlign: 'right', 
   },
   
-  directorTitle: {
+  tituloDiretor: { 
     color: '#377A98',
     marginLeft: 20,
     marginTop: 20,
     fontSize: 20,
   },
-  directorInfoRow: {
+  linhaInfoDiretor: { 
     flexDirection: 'row',
     alignItems: 'center',
     marginLeft: 20,
     marginTop: 5,
   },
-  directorImage: {
+  imagemDiretor: { 
     width: 50,
     height: 50,
     resizeMode: 'contain',
     marginRight: 10,
   },
-  directorFilmText: {
+  textoNomeDiretor: { 
     color: '#69C6AF',
   },
-  test: {
+  containerTeste: { 
     marginLeft: 190,
   },
-  
+  containerTeste2: { 
+    marginTop: -230,
+  },
 });
