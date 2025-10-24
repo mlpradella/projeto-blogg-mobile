dcfe30163039d183635f97bf2612e84ef1a017f7
mlpradella <ribeiro.santos.maria0406@escola.pr.gov.br>
2025-10-24 12:08:23 -0300

    coloquei os textos do primeiro filme

--- mobile/app/(tabs)/romace.tsx
+++ mobile/app/(tabs)/romace.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView, SafeAreaViewBase, StyleSheet, View, } from 'react-native'
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { ScrollView } from 'react-native-gesture-handler';
+import { NavigationContainerRefContext } from '@react-navigation/native';
 
 export default function HomeScreen() {
   return (
@@ -15,8 +16,8 @@ export default function HomeScreen() {
             <ThemedText type="title" style={styles.titleContainer}>Romance</ThemedText>
           </ThemedView>
 
-          <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle">Simplismente Acontece</ThemedText>
+          <ThemedView style={styles.titulo1}>
+            <ThemedText type="default">Simplismente Acontece</ThemedText>
           </ThemedView>
 
           <Image
@@ -29,10 +30,18 @@ export default function HomeScreen() {
                   style={styles.informacoesSimp}
                   />
 
+                  <ThemedView style={styles.containerConteudo}></ThemedView>
+
           <ThemedView style={styles.texto}>
-            <ThemedText style={styles.opiniao}
-          type='default'>A história é até interessante, algumas pessoas podem achar o enredo um pouco previsível para o gênero, mas o filme se destaca por não cair em todos os clichês. 
-        Pois o filme mostra uma história sobre as complicações da vida real e como o amor pode ser mais uma questão de tempo do que de destino. 
+            <ThemedText style={styles.sinopse}
+          type='default'>Alex e Rosie são amigos inseparáveis que cresceram juntos em Londres.
+                        Mas, tudo muda quando Alex ganha uma bolsa de estudos e se muda para os EUA. 
+                        Agora trilhando caminhos diferentes. Porém, o destino faz com que se encontrem novamente.</ThemedText>
+       </ThemedView>
+       <ThemedView style={styles.texto}>
+        <ThemedText style={styles.opiniao}
+        type='default'>A história é até interessante, algumas pessoas podem achar o enredo um pouco previsível para o gênero, 
+        mas o filme se destaca por não cair em todos os clichês. Pois o filme mostra uma história sobre as complicações da vida real e como o amor pode ser mais uma questão de tempo do que de destino. 
         No final, é um filme leve, divertido e emocionante, perfeito para quem gosta de um romance que faz rir, e de vez em quando, soltar uma lágrima.</ThemedText>
        </ThemedView>
     </View>
@@ -49,7 +58,7 @@ const styles = StyleSheet.create({
       },
 
       titleContainer: {
-        color: '#FF6992',
+        color: '#EF124F',
         alignItems: 'center',
         gap: 8,
         backgroundColor: '#00002B',
@@ -57,11 +66,13 @@ const styles = StyleSheet.create({
         fontFamily: 'Bungee',
       },
 
-      stepContainer: {
+      titulo1: {
         gap: 8,
         marginBottom: 8,
         backgroundColor: '#00002B',
         alignItems: 'center',
+        fontFamily: 'ui-sans-serif',
+        color: '#FF6992',
       },
 
       reactLogo: {
@@ -75,12 +86,6 @@ const styles = StyleSheet.create({
       texto: {
       backgroundColor: '#00002B',
       fontSize: 12,
-      },
-
-      opiniao: {
-        fontSize:16,
-        marginLeft: 10, 
-        marginTop: 35,
       },  
 
       scrollContent:{
@@ -89,22 +94,43 @@ const styles = StyleSheet.create({
       },
 
       newImageStyle:{
-        height: 270,
-        width: 150 , 
-        resizeMode: 'contain',
-        paddingTop: 5, 
-        marginLeft: 10,
-        alignItems: 'flex-start',
+         width: 150,
+         height: 225, 
+         resizeMode: 'contain',    
+         marginLeft: 6,       
+         borderRadius: 8,
+         top: 10,
+      },
+
+      containerConteudo: {
+            flex: 1,
+            flexDirection: 'column',
       },
 
        informacoesSimp: {
-        height: 290,
-        width: 120,
-        resizeMode:'contain',
-        marginLeft: 190,
-        marginTop: -310,
-        alignItems: 'flex-start',
+        width: 150,     
+        height: 83,    
+        resizeMode: 'contain',
+        marginBottom: 10,
+        marginLeft: 130,
+        top: -205,
       },
+
+      sinopse: {
+        fontSize: 8.8,
+        marginLeft: 161, 
+        top: -216,
+        maxWidth: 600,
+            
+      },
+
+      opiniao: {
+        fontSize: 8.7,
+        marginLeft: 5, 
+        top: -197,
+        maxWidth: 1860,
+        height:'100%',
+      }
 
     })
 
