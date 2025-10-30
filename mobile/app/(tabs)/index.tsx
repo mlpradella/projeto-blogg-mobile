e47b6234e2e2e8d0bc032fcb42cf893551065421
gigioo-00 <giovannagabriellyy001@gmail.com>
2025-10-30 12:09:36 -0300

    ajustei os cards com os seus respectivos textos, cores e botões

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 import { useSharedValue } from 'react-native-reanimated';
 import Carousel from 'react-native-reanimated-carousel';
 import { ThemedText } from '@/components/themed-text';
@@ -7,52 +7,97 @@ import { ThemedView } from '@/components/themed-view';
 import { window } from '@/constants/sizes';
 import { useRouter } from 'expo-router';
 
-const imageData = [
-  { src: require('@/assets/images/verde.png'), route: '/(tabs)/acao' },
-  { src: require('@/assets/images/roxo.png'), route: '/(tabs)/animacao' },
-  { src: require('@/assets/images/azul.png'), route: '/(tabs)/drama' },
-  { src: require('@/assets/images/rosa.webp'), route: '/(tabs)/romace' },
-  { src: require('@/assets/images/vermelho.png'), route: '/(tabs)/terror' },
+const cardData = [
+  {
+    title: 'AÇÃO',
+    description:
+      'Nada me empolga mais do que explosões, fugas e aventuras arriscadas! Cada cena me faz sentir a adrenalina correr nas veias! Neste post, você vai conhecer meus filmes de ação favoritos.',
+    author: 'Maria Luiza',
+    color: '#95b963',
+    route: '/(tabs)/acao',
+  },
+  {
+    title: 'ANIMAÇÃO',
+    description:
+      'É incrível como cada história animada consegue ensinar e encantar ao mesmo tempo. Te convido a ver meus filmes de animação preferidos para passar o tempo. ^^',
+    author: 'Bianca Amorim',
+    color: '#7d5b8c',
+    route: '/(tabs)/animacao',
+  },
+  {
+    title: 'DRAMA',
+    description:
+      'Filmes de drama falam sobre a vida real, sobre sentimentos e decisões que mudam tudo. Acho que se quer sentir uma dor emocional, meu post é para você...',
+    author: 'Allan',
+    color: '#5391c7',
+    route: '/(tabs)/drama',
+  },
+  {
+    title: 'ROMANCE',
+    description:
+      'Os filmes de romance me fazem sonhar, sorrir e até derramar algumas lágrimas. Vem se apaixonar comigo conhecendo aos meus filmes de romance preferidos <3',
+    author: 'Kamilly',
+    color: '#e994b7',
+    route: '/(tabs)/romace',
+  },
+  {
+    title: 'TERROR',
+    description:
+      'Quanto mais suspense e mistério, melhor! Gosto de mergulhar no clima sombrio e descobrir o que se esconde nas sombras. Se você tem esse sentimento vendo filmes de terror, conheça minhas recomendações!!',
+    author: 'Maykon',
+    color: '#a82223',
+    route: '/(tabs)/terror',
+  },
 ];
 
 export default function HomeScreen() {
   const progress = useSharedValue<number>(0);
   const router = useRouter();
 
-  const renderItem = React.useCallback(({ item }: { item: any; index: number }) => {
-    return (
-      <TouchableOpacity
-        style={styles.card}
-        activeOpacity={0.8}
-        onPress={() => router.push(item.route)}
-      >
-        <Image source={item.src} style={styles.image} resizeMode="cover" />
-      </TouchableOpacity>
-    );
-  }, [router]);
+  const renderItem = React.useCallback(
+    ({ item }: { item: any }) => (
+      <View style={[styles.card, { backgroundColor: item.color }]}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.author}>by {item.author}</Text>
+
+        <TouchableOpacity
+          style={styles.buttonCard}
+          activeOpacity={0.8}
+          onPress={() => router.push(item.route)}
+        >
+          <Text style={styles.buttonText}>Ver Post</Text>
+        </TouchableOpacity>
+      </View>
+    ),
+    [router]
+  );
 
   return (
     <View style={styles.container}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="subtitle">new blog!</ThemedText>
-        <ThemedText type="title" style={styles.fonte}>POCSCAST</ThemedText>
+        <ThemedText type="title" style={styles.fonte}>
+          POCSCAST
+        </ThemedText>
         <ThemedText type="subtitle">MOVIES</ThemedText>
-        <ThemedText type="subtitle" style={styles.subtitulo}>principais destaques</ThemedText>
+        <ThemedText type="subtitle" style={styles.subtitulo}>
+          principais destaques
+        </ThemedText>
       </ThemedView>
 
       <View style={styles.carouselContainer}>
         <Carousel
-          autoPlayInterval={2000}
-          data={imageData}
-          height={550}
+          autoPlayInterval={2500}
+          data={cardData}
+          height={520}
           loop
           pagingEnabled
           snapEnabled
           width={window.width}
-          style={{ width: window.width }}
           mode="parallax"
           modeConfig={{
-            parallaxScrollingScale: 0.7,
+            parallaxScrollingScale: 0.8,
             parallaxScrollingOffset: 50,
           }}
           onProgressChange={progress}
@@ -91,44 +136,74 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignItems: 'center',
-    backgroundColor: '#00002b',
+    backgroundColor: '#00002B',
     paddingVertical: 1,
   },
   carouselContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 100, 
-    marginBlockEnd:65,
-    
+    marginTop: 100,
+    marginBottom: 60,
   },
   card: {
     flex: 1,
-    marginHorizontal: 20,
-    borderRadius: 15,
-    overflow: 'hidden',
+    marginHorizontal: 30,
+    borderRadius: 20,
+    padding: 25,
+    justifyContent: 'space-between',
     shadowColor: '#000',
     shadowOffset: { width: 2, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
   },
-  image: {
+  title: {
+    fontSize: 30,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  description: {
+    color: '#fff',
+    fontSize: 30,
+    textAlign: 'center',
+    lineHeight: 30,
+    flexGrow: 1,
+  },
+  author: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  buttonCard: {
     width: '100%',
-    height: '100%',
-    borderRadius: 15,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    borderRadius: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
   recommendationContainer: {
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 20, 
-    marginBottom: 40,
+    marginTop: 30,
+    marginBottom: 25,
+
   },
   recommendationText: {
     color: '#FFFFFF',
     fontSize: 16,
     marginBottom: 10,
-    marginTop:10,
+    marginTop: 10,
   },
   button: {
     borderWidth: 1,
@@ -136,10 +211,5 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingVertical: 10,
     paddingHorizontal: 30,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 15,
-    textAlign: 'center',
-  },
+  }
 });
